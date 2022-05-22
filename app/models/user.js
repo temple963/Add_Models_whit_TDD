fa821c50e49e765b0254467674779586dab438d6
Humberto_Ruz <Ruz_s_92@hotmail.com>
2022-05-22 14:16:06 -0500

    el orden de las variables enel objeto altera el resultado

---
+++ app/models/user.js
@@ -0,0 +1,15 @@
+class User {
+
+    constructor(id,name,username,bio,dataCreated,lastUpdated){
+
+        this.id=id
+        this.username=name
+        this.name=username
+        this.bio=bio
+        this.dataCreated=dataCreated
+        this.lastUpdated=lastUpdated
+
+    }
+}
+
+module.exports = User
